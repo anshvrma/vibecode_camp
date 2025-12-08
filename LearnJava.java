9107f1c7b2220d2084fe8dbafb564894d5d33d65
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2025-12-08 17:15:39 +0530

    first module done

--- LearnJava.java
+++ LearnJava.java
@@ -620,7 +620,20 @@ public class LearnJava {
         // }
 
         //pattern 20
+        // int n = scn_num.nextInt();
         
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (j == 1 || j == n) {
+        //             System.out.print("*\t");
+        //         } else if (i > n/2 && (i == j || i + j == n + 1)) {
+        //                 System.out.print("*\t");
+        //         } else {
+        //                 System.out.print("\t");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
 
 
 
