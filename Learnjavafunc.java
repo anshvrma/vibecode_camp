e9b423daa65f48ae017c2bd3cd67f8970111a2df
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2025-12-16 16:23:40 +0530

    getDecimal

--- Learnjavafunc.java
+++ Learnjavafunc.java
@@ -71,7 +71,29 @@ public class Learnjavafunc {
     //     return rv;
     // }
 
-    
+    //Any baser to decimal
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = scn.nextInt();
+        System.out.println("Enter the base number: ");
+        int b = scn.nextInt();
+        int res = getDecimal(n, b);
+        System.out.println(res);
+    }
+
+    public static int getDecimal(int n, int b){
+        int rv = 0;
+        int p = 1;
+        while(n > 0){
+            int dig = n % 10;
+            n = n/10;
+            rv += dig * p;
+            p  = p * b;
+        }
+
+        return rv;
+    }
 
 
 
