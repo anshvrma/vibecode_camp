5821d6eae5606a6d2c7a4e7eef759d76b86cb843
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-12 16:27:31 +0530

    BasicofStack

--- LearnJava/Learnjavastk.java
+++ LearnJava/Learnjavastk.java
@@ -9,12 +9,26 @@ public class Learnjavastk {
     //     Stack<Integer> st = new Stack<>();
 
     //     st.push(10);
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
     //     st.push(20);
-    //     System.out.println(st);
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
     //     st.push(30);
-    //     System.out.println(st);
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
     //     st.push(40);
-    //     System.out.println(st);
-    //     System.out.println(st.peek() + " " + st.size());
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
+    //     st.pop();
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
+    //     st.pop();
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
+    //     st.pop();
+    //     System.out.println(st + "->" + st.peek() + " " + st.size());
+    //     st.pop();
+    //     System.out.println(st + "->" + st.size());
     // }
+
+    
+
+
+
+
 }
