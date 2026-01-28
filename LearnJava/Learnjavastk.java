bdb8edf54899b7fc0c9db6a784b5469686d448ff
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-28 16:21:24 +0530

    PostfixEvaluation

--- LearnJava/Learnjavastk.java
+++ LearnJava/Learnjavastk.java
@@ -465,7 +465,58 @@ public class Learnjavastk {
     //     }
     // }
 
+    //Postfix Evaluation
+    // public static void main(String[] args) throws Exception {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     String exp = br.readLine();
+
+    //     Stack<String> prefix = new Stack<>();
+    //     Stack<String> infix = new Stack<>();
+    //     Stack<Integer> valstk = new Stack<>();
+
+    //     for(int i = 0; i < exp.length(); i++){
+    //         char ch = exp.charAt(i);
+
+    //         if(ch == '+' || ch == '-' || ch == '*' || ch == '/'){
+    //             int v2 = valstk.pop();
+    //             int v1 = valstk.pop();
+    //             int val = operation(v1, v2, ch);
+    //             valstk.push(val);
+
+    //             String iv2 = infix.pop();
+    //             String iv1 = infix.pop();
+    //             String ival = "(" + iv1 + ch + iv2 + ")";
+    //             infix.push(ival);
+
+    //             String pv2 = prefix.pop();
+    //             String pv1 = prefix.pop();
+    //             String pval = ch + pv1 + pv2;
+    //             prefix.push(pval);
+    //         } else{
+    //             valstk.push(ch - '0');
+    //             prefix.push(ch + "");
+    //             infix.push(ch + "");
+    //         }
+    //     }
+
+    //     System.out.println(valstk.pop());
+    //     System.out.println(infix.pop());
+    //     System.out.println(prefix.pop());
+    // }
+    // public static int operation(int v1, int v2, char op){
+    //     if(op == '+'){
+    //         return v1 + v2;
+    //     } else if (op == '-'){
+    //         return v1 - v2;
+    //     } else if (op == '*'){
+    //         return v1 * v2;
+    //     } else {
+    //         return v1 / v2;
+    //     }
+    // }
+
     
+
 
 
 
